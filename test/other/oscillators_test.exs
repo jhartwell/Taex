8b7847bba47cf9b06d1962f37a05053aa0b567cc
Behnam Shomali <behnam.shomalis@tretton37.com>
2019-03-26 11:41:51 +0100

    Refactor OscillatorsTests

--- test/other/oscillators_test.exs
+++ test/other/oscillators_test.exs
@@ -1,13 +1,15 @@
 defmodule Taex.OscillatorsTests do
     use ExUnit.Case
   
+    @prices [82.15,81.89,83.03,83.30,83.85,83.90,83.33,84.30,84.84,85.00,85.90,86.58,86.98,88.00,87.87]
+
     alias Taex.Oscillators
     test "stochastic" do
-      assert Oscillators.stochastic([82.15,81.89,83.03,83.30,83.85,83.90,83.33,84.30,84.84,85.00,85.90,86.58,86.98,88.00,87.87]) == 97.872340425532
+      assert Oscillators.stochastic(@prices) == 97.872340425532
     end
 
     test "stocastic_ma" do
-      assert Oscillators.stocastic_ma([85.00,85.90,86.58,86.98,88.00,87.87]) == 85.82666666666667
+      assert Oscillators.stocastic_ma(@prices) == 82.35666666666667
     end
 
   end
