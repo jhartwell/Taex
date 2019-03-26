b0a369173d4c85a891b6e34fffc06c4c171f908f
Behnam Shomali <behnam.shomalis@tretton37.com>
2019-03-26 10:49:21 +0100

    Add unit tests for  Taex.Helper
    Add unit tests for high and low methods with 2 arguments

--- test/other/other_test.exs
+++ test/other/other_test.exs
@@ -6,11 +6,13 @@ defmodule Taex.Other do
     assert Helpers.high([1,2,3,4,5]) == 5
     assert Helpers.high(5)== 5
     assert Helpers.high([1,1,4,2,1,5]) == 5
+    assert Helpers.high(3, [3,2,5,1,232,15,4,3]) == 15
   end
 
   test "low helper" do
     assert Helpers.low([1,2,3,4,5]) == 1
     assert Helpers.low(1) == 1
     assert Helpers.low([1,4,2,6,8,234,1,2,4323]) == 1
+    assert Helpers.low(3, [3,2,5,1,232,15,4,3]) == 3
   end
 end
