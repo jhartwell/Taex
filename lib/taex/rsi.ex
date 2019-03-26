eea161ab691b132201ece3e676137e5ff8a91f51
Behnam Shomali <behnam.shomalis@tretton37.com>
2019-03-26 16:32:08 +0100

    Add averageGain method to RSI module

--- lib/taex/rsi.ex
+++ lib/taex/rsi.ex
@@ -4,8 +4,10 @@ defmodule Taex.RSI do
     1
   end
 
+  def averageGain([]), do: 0
   def averageGain(prices) when is_list(prices) do
-    
+    totalGaines = gain(prices)
+    totalGaines / Enum.count(prices)
   end
 
   def averageLoss([]), do: 0
@@ -16,6 +18,10 @@ defmodule Taex.RSI do
 
   defp loss([_]), do: 0
   defp loss([head|tail]) when hd(tail) >= head, do: loss(tail)
-  defp loss([head|tail]) when hd(tail)< head, do: head - hd(tail) + loss(tail)
+  defp loss([head|tail]) when hd(tail) < head, do: head - hd(tail) + loss(tail)
+
+  defp gain([_]), do: 0
+  defp gain([head|tail]) when hd(tail) <= head, do: gain(tail)
+  defp gain([head|tail]) when hd(tail) > head,  do: hd(tail) - head + gain(tail)
 
 end
--- test/other/rsi_test.exs
+++ test/other/rsi_test.exs
@@ -2,6 +2,12 @@ defmodule Taex.RsiTests do
   use ExUnit.Case
   alias Taex.RSI
 
+  test "average gain" do
+    assert RSI.averageGain([10, 20, 30, 40]) == 7.5
+    assert RSI.averageGain([3, 2, 1]) == 0
+    assert RSI.averageGain([]) == 0
+  end
+
   test "average loss" do
     assert RSI.averageLoss([30, 20, 10, 0]) == 7.5
     assert RSI.averageLoss([1, 2, 3]) == 0
